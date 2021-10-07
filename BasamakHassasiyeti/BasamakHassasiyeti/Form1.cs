2d06bcef395844b4aee10c08e6605636527232b6
ismaildedec <ismaildedec@gmail.com>
2021-10-07 21:31:09 +0300

    Farkli degisken tipleri tanimlama örnegi eklendi.

---
+++ BasamakHassasiyeti/BasamakHassasiyeti/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasamakHassasiyeti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float sayi1;
+            double sayi2;
+            decimal sayi3;
+            sayi1 = 1234567890123456789;
+            sayi2 = 1234567890123456789;
+            sayi3 = 1234567890123456789;
+            MessageBox.Show(sayi1.ToString(), "Sayi1' in Değeri");
+            MessageBox.Show(sayi2.ToString(), "Sayi2' in Değeri");
+            MessageBox.Show(sayi3.ToString(), "Sayi3' in Değeri");
+        }
+    }
+}
