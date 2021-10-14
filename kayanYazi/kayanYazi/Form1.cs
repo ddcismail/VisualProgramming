8c10629b550cf774ae77ab6ef4385d5cf56aa3c0
ismaildedec <ismaildedec@gmail.com>
2021-10-14 11:08:35 +0300

    Kayan yazi ornegi eklendi.

---
+++ kayanYazi/kayanYazi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kayanYazi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 500; //yazının kayma hızı ayarlandı
+            timer1.Enabled = true; //timer kontrolü çalıştırıldı.
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //texBox kontrolünde bulunan yazının ilk karakteri alınıp en sona ekleme işlemi yapılıyor.
+            textBox1.Text = textBox1.Text.Substring(1) + textBox1.Text.Substring(0, 1);
+        }
+    }
+}
