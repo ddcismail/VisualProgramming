5d9a93cef8e32f6f648adb9b21b485abdf42db9b
ismaildedec <ismaildedec@gmail.com>
2021-10-29 13:56:41 +0300

    Sub String ornegi eklendi.

---
+++ subString/Sayfa10_subString/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayfa10_subString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ifade = "Düzce";
+            string kesim = "";
+            kesim = ifade.Substring(3, 2);
+            MessageBox.Show(kesim);
+        }
+    }
+}
---
+++ subString/Sayfa10_subString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayfa10_subString
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
