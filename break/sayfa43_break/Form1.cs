2b8d0e321f09509238580aefa9a5c4b9a6c5df1a
ismaildedec <ismaildedec@gmail.com>
2021-11-04 23:58:40 +0300

    Break örneği eklendi.

---
+++ break/sayfa43_break/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa43_break
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] isimler = new string[5] { "İsmail", "Bugra", "Ahmet", "Mehmet", "Ayşe" };
+            string aranan_isim = "İsmail";
+
+            int i;
+            for (i = 0; i < isimler.Length; i++)
+            {
+                if (aranan_isim == isimler[i])
+                {
+                    break;
+                }
+            }
+            MessageBox.Show(isimler[i] + " isimli kişi dizide " + (i + 1) + ". sıradadır.");
+
+        }
+    }
+}
