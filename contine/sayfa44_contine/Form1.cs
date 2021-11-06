99a2e2682766f9709714981eb9178235ccb7c755
ismaildedec <ismaildedec@gmail.com>
2021-11-06 23:42:39 +0300

    Contine örneği eklendi.

---
+++ contine/sayfa44_contine/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa44_contine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 4 != 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    listBox1.Items.Add(i.ToString());
+                }
+            }
+        }
+    }
+}
