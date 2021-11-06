0f4cacfe408f6f744d80198194a88ebf77a3209f
ismaildedec <ismaildedec@gmail.com>
2021-11-06 23:46:45 +0300

    While'da sonsuz döngü örneği eklendi.

---
+++ while_sonsuzDongu/sayfa44_while_sonsuzDongu/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace sayfa44_while_sonsuzDongu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sonsuz Döngüyü Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sonsuz Döngüyü Sonlandır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 332);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 406);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ while_sonsuzDongu/sayfa44_while_sonsuzDongu/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa44_while_sonsuzDongu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool tiklandimi;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tiklandimi = false;
+            int i = 0;
+            int sayi_durum = 0;
+            while (i < 10)
+            {
+                sayi_durum++;
+                label1.Text = sayi_durum.ToString();
+                Application.DoEvents();
+                if (tiklandimi == true)
+                {
+                    break;
+                }
+            }
+            MessageBox.Show(sayi_durum.ToString() + ". sayıda sonsuz döngüden çıkılmıştır.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tiklandimi = true;
+        }
+    
+    }
+}
