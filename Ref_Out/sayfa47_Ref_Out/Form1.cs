3a7cfbbbf74d8ebcf67afbb0219753b937cf8bfb
ismaildedec <ismaildedec@gmail.com>
2021-11-06 23:45:25 +0300

    Ref_out örneği eklendi.

---
+++ Ref_Out/sayfa47_Ref_Out/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa47_Ref_Out
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void deger(int sayi1, int sayi2, ref int sayi3)
+        {
+            sayi1 = 100;
+            sayi2 = 200;
+            sayi3 = 300;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1 = 5, s2 = 10, s3 = 15;
+            deger(s1, s2, ref s3);
+            MessageBox.Show(s1.ToString() + "\t" + s2.ToString() + "\t" + s3.ToString());
+
+        }
+    }
+}
---
+++ Ref_Out/sayfa47_Ref_Out/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa47_Ref_Out
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
