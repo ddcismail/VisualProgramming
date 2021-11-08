8d9e5fd775a7f1de235b1ddadb1683b0a7fc3153
ismaildedec <ismaildedec@gmail.com>
2021-11-08 13:07:46 +0300

    Büyük bulan uygulama örnegi eklendi.

---
+++ BuyukBulma/sayfa48_BuyukBulma/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace sayfa48_BuyukBulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "40 ve 20";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BÜYÜGÜ BUL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 236);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BuyukBulma/sayfa48_BuyukBulma/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa48_BuyukBulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static int max(int sayi1, int sayi2)
+        {
+            int donendeger = 0; ;
+            if (sayi1 > sayi2)
+            {
+                donendeger = sayi1;
+            }
+            else if (sayi2 > sayi1)
+            {
+                donendeger = sayi2;
+            }
+            return donendeger;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gelensayi = max(40, 20);
+            MessageBox.Show(gelensayi.ToString());
+        }
+    }
+}
