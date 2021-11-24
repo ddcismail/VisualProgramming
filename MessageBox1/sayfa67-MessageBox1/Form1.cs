88fbe7bfb02b6afba0cb554de474443674641e0d
ismaildedec <ismaildedec@gmail.com>
2021-11-24 17:03:41 +0300

    MessageBox01 örneği eklendi.

---
+++ MessageBox1/sayfa67-MessageBox1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa67_MessageBox1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult a;
+            a = MessageBox.Show("Form Kapatılsın mı?", "Onay", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            
+            if (a == DialogResult.Yes)
+            {
+                this.Close();
+            }
+
+        }
+    }
+}
