52d19a904e818459361a6910f3e6c24d398146da
ismaildedec <ismaildedec@gmail.com>
2021-11-24 16:59:37 +0300

    MessageBox.Show(M- B- D- S- V-D) örneği eklendi.

---
+++ MessageBox.Show(M- B- D- S- V-D)/sayfa66-MessageBox.Show(M- B- D- S- V-D)/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa66_MessageBox.Show_M__B__D__S__V_D_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çıkamak İstediğinizden Emin Misiniz?", "ONAY", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);//MessageBoxDefaultButton ile seçili button üzerinde vurgulama yapılır.
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dosyayı Silmek İstiyor Musunuz?", "ONAY", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+        }
+    }
+}
