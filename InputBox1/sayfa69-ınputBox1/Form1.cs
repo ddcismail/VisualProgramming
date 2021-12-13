da6c3f7cd8ca703081446966f34913aba8c4c5e2
ismaildedec <ismaildedec@gmail.com>
2021-12-13 22:32:45 +0300

    InputBox01 örnek eklendi.

---
+++ InputBox1/sayfa69-ınputBox1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa69_ınputBox1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sinav_notu;
+            sinav_notu = Convert.ToInt16(Microsoft.VisualBasic.Interaction.InputBox("Sınav Notu Girin", "Not Girişi", "0", 100, 100));
+
+            if (sinav_notu >= 50)
+            {
+                MessageBox.Show("Tebrikler. Dersten Geçtiniz");
+            }
+            else
+            {
+                MessageBox.Show("Üzgünüm. Dersten kaldınız.");
+            }
+
+        }
+    }
+}
