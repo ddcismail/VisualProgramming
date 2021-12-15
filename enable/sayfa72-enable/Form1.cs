0002941b5253f3369328f0b15fbc9bcb4c07d506
ismaildedec <ismaildedec@gmail.com>
2021-12-15 07:58:24 +0300

    Enable örneği eklendi.

---
+++ enable/sayfa72-enable/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa72_enable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            MessageBox.Show("Programla bağlantı sağlandı. ", "Bağlantı Durumu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            MessageBox.Show("Programla bağlantı sonlandı. ", "Bağlantı Durumu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
