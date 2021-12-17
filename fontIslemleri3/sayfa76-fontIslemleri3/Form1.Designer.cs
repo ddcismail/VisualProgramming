81177e691bf1fa45d8d21d06235a8f38c11fdcfc
ismaildedec <ismaildedec@gmail.com>
2021-12-17 07:54:22 +0300

    Font işlemleri03 örneği eklendi.

---
+++ fontIslemleri3/sayfa76-fontIslemleri3/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace sayfa76_fontIslemleri3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_yazi = new System.Windows.Forms.TextBox();
+            this.listBox_fontlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txt_yazi
+            // 
+            this.txt_yazi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_yazi.Location = new System.Drawing.Point(211, 145);
+            this.txt_yazi.Multiline = true;
+            this.txt_yazi.Name = "txt_yazi";
+            this.txt_yazi.Size = new System.Drawing.Size(305, 92);
+            this.txt_yazi.TabIndex = 3;
+            this.txt_yazi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // listBox_fontlar
+            // 
+            this.listBox_fontlar.FormattingEnabled = true;
+            this.listBox_fontlar.Location = new System.Drawing.Point(35, 25);
+            this.listBox_fontlar.Name = "listBox_fontlar";
+            this.listBox_fontlar.Size = new System.Drawing.Size(154, 407);
+            this.listBox_fontlar.TabIndex = 2;
+            this.listBox_fontlar.SelectedIndexChanged += new System.EventHandler(this.listBox_fontlar_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_yazi);
+            this.Controls.Add(this.listBox_fontlar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_yazi;
+        private System.Windows.Forms.ListBox listBox_fontlar;
+    }
+}
+
---
+++ fontIslemleri3/sayfa76-fontIslemleri3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa76_fontIslemleri3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FontFamily[] fontlar = FontFamily.Families;
+            for (int i = 0; i < fontlar.Length; i++)
+            {
+                listBox_fontlar.Items.Add(fontlar[i].Name);
+            }
+
+        }
+
+        private void listBox_fontlar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txt_yazi.Font = new Font(listBox_fontlar.SelectedItem.ToString(), 16, FontStyle.Regular);
+            }
+            catch
+            {
+                MessageBox.Show("Bu font görüntelenemiyor.");
+            }
+
+        }
+    }
+}
