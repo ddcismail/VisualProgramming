12378be0197d51848afe55714ea200ffb49dce94
ismaildedec <ismaildedec@gmail.com>
2021-12-19 02:24:09 +0300

    Kordinant05 örneği eklendi.

---
+++ koordinant_Boyut5/sayfa83-koordinant_Boyut5/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace sayfa83_koordinant_Boyut5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button18 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.groupBox3.Controls.Add(this.button13);
+            this.groupBox3.Controls.Add(this.button14);
+            this.groupBox3.Controls.Add(this.button15);
+            this.groupBox3.Controls.Add(this.button16);
+            this.groupBox3.Controls.Add(this.button17);
+            this.groupBox3.Controls.Add(this.comboBox3);
+            this.groupBox3.Controls.Add(this.button18);
+            this.groupBox3.Location = new System.Drawing.Point(55, 172);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(691, 100);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(359, 35);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(62, 39);
+            this.button13.TabIndex = 11;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(291, 35);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(62, 39);
+            this.button14.TabIndex = 10;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(223, 35);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(62, 39);
+            this.button15.TabIndex = 9;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(155, 35);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(62, 39);
+            this.button16.TabIndex = 8;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(87, 35);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(62, 39);
+            this.button17.TabIndex = 7;
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(457, 45);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(19, 35);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(62, 39);
+            this.button18.TabIndex = 0;
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button11);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Location = new System.Drawing.Point(55, 323);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(485, 100);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(223, 35);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(62, 39);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(155, 35);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(62, 39);
+            this.button10.TabIndex = 8;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(87, 35);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(62, 39);
+            this.button11.TabIndex = 7;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(320, 45);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(19, 35);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(62, 39);
+            this.button12.TabIndex = 0;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(55, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(691, 100);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(359, 35);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(62, 39);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(291, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 39);
+            this.button5.TabIndex = 10;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(223, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(62, 39);
+            this.button4.TabIndex = 9;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(155, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 39);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 39);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(457, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 39);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ koordinant_Boyut5/sayfa83-koordinant_Boyut5/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa83_koordinant_Boyut5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ifadeler = { "Yok", "Sağa", "Sola", "Üste", "Alta", "İçine" };
+            comboBox1.Items.AddRange(ifadeler);
+            comboBox2.Items.AddRange(ifadeler);
+            comboBox3.Items.AddRange(ifadeler);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Yok")
+            {
+                groupBox1.Dock = DockStyle.None;
+            }
+            else if (comboBox1.Text == "Sağa")
+            {
+                groupBox1.Dock = DockStyle.Right;
+            }
+            else if (comboBox1.Text == "Sola")
+            {
+                groupBox1.Dock = DockStyle.Left;
+            }
+            else if (comboBox1.Text == "Üste")
+            {
+                groupBox1.Dock = DockStyle.Top;
+            }
+            else if (comboBox1.Text == "Alta")
+            {
+                groupBox1.Dock = DockStyle.Bottom;
+            }
+            else if (comboBox1.Text == "İçine")
+            {
+                groupBox1.Dock = DockStyle.Fill;
+            }
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text == "Yok")
+            {
+                groupBox3.Dock = DockStyle.None;
+            }
+            else if (comboBox3.Text == "Sağa")
+            {
+                groupBox3.Dock = DockStyle.Right;
+            }
+            else if (comboBox3.Text == "Sola")
+            {
+                groupBox3.Dock = DockStyle.Left;
+            }
+            else if (comboBox3.Text == "Üste")
+            {
+                groupBox3.Dock = DockStyle.Top;
+            }
+            else if (comboBox3.Text == "Alta")
+            {
+                groupBox3.Dock = DockStyle.Bottom;
+            }
+            else if (comboBox3.Text == "İçine")
+            {
+                groupBox3.Dock = DockStyle.Fill;
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Yok")
+            {
+                groupBox2.Dock = DockStyle.None;
+            }
+            else if (comboBox2.Text == "Sağa")
+            {
+                groupBox2.Dock = DockStyle.Right;
+            }
+            else if (comboBox2.Text == "Sola")
+            {
+                groupBox2.Dock = DockStyle.Left;
+            }
+            else if (comboBox2.Text == "Üste")
+            {
+                groupBox2.Dock = DockStyle.Top;
+            }
+            else if (comboBox2.Text == "Alta")
+            {
+                groupBox2.Dock = DockStyle.Bottom;
+            }
+            else if (comboBox2.Text == "İçine")
+            {
+                groupBox2.Dock = DockStyle.Fill;
+            }
+
+        }
+    }
+}
