62bea5d901b7f9f510591ab0f813daa22a32daf6
ismaildedec <ismaildedec@gmail.com>
2021-12-22 01:42:06 +0300

    Forma ait baslangıc form ornegi eklendi.

---
+++ forma_Ait_Olaylar_baslangic_form/sayfa106-forma_Ait_Olaylar_baslangic_form/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa106_forma_Ait_Olaylar_baslangic_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 bizim_ikinci_formumuz = new Form2();
+            bizim_ikinci_formumuz.Show();
+
+        }
+    }
+}
