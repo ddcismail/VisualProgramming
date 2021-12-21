aa77b83e7699287434a41b172e726ee9cc5aefab
ismaildedec <ismaildedec@gmail.com>
2021-12-21 12:49:11 +0300

    Forma ait mototlarla uygulama örneği eklendi.

---
+++ forma_Ait_Metotlar/sayfa105-forma_Ait_Metotlar/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa105_forma_Ait_Metotlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int orijinal_x = 800;
+        int orijinal_y = 600;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Rectangle cozunurluk = new Rectangle();
+            cozunurluk = Screen.GetBounds(cozunurluk);
+            float oran_x, oran_y;
+            oran_x = (float)cozunurluk.Width / orijinal_x;
+            oran_y = (float)cozunurluk.Height / orijinal_y;
+            this.Left = 0;
+            this.Top = 0;
+            this.Scale(oran_x, oran_y);
+
+        }
+    }
+}
