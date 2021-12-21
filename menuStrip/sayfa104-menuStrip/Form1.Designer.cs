64ed7ca7568e8d496d102f31a9e0090e3f49d858
ismaildedec <ismaildedec@gmail.com>
2021-12-21 12:48:10 +0300

    Menustript uygulama örneği eklendi.

---
+++ menuStrip/sayfa104-menuStrip/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace sayfa104_menuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kontrolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kontrolToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kontrolToolStripMenuItem
+            // 
+            this.kontrolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem,
+            this.silToolStripMenuItem});
+            this.kontrolToolStripMenuItem.Name = "kontrolToolStripMenuItem";
+            this.kontrolToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.kontrolToolStripMenuItem.Text = "Kontrol";
+            this.kontrolToolStripMenuItem.Click += new System.EventHandler(this.kontrolToolStripMenuItem_Click);
+            // 
+            // ekleToolStripMenuItem
+            // 
+            this.ekleToolStripMenuItem.Name = "ekleToolStripMenuItem";
+            this.ekleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ekleToolStripMenuItem.Text = "Ekle";
+            this.ekleToolStripMenuItem.Click += new System.EventHandler(this.ekleToolStripMenuItem_Click_1);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kontrolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+    }
+}
+
---
+++ menuStrip/sayfa104-menuStrip/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa104_menuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int say, y = 30;
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Button komut_dugmesi = new Button();
+            komut_dugmesi.Top = y;
+            komut_dugmesi.Left = 0;
+            komut_dugmesi.Width = 150;
+            say++;
+            komut_dugmesi.Text = "Yeni Komut Düğmesi " + say.ToString();
+            y += komut_dugmesi.Height + 5;
+
+            komut_dugmesi.Click += new EventHandler(tıkla);
+
+            this.Controls.Add(komut_dugmesi);
+
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(this.ActiveControl);
+
+        }
+
+        private void kontrolToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ekleToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tıkla(object sender, System.EventArgs e)
+        {
+            MessageBox.Show((sender as Button).Text + " tıklandı");
+        }
+    }
+}
