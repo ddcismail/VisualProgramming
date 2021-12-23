7f1fa9cf87859c98b48534d9e57afe4e5dfcdae2
ismaildedec <ismaildedec@gmail.com>
2021-12-23 11:56:26 +0300

    TextBox1 modifiers örneği eklendi.

---
+++ textBox1_modifiers/sayfa111-textBox1_modifiers/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa111_textBox1_modifiers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 bizim_ikinci_formumuz = new Form2();
+            bizim_ikinci_formumuz.x = 100;
+            bizim_ikinci_formumuz.textBox1.Text = "X in degeri = 100";
+            bizim_ikinci_formumuz.Show();
+
+        }
+    }
+}
---
+++ textBox1_modifiers/sayfa111-textBox1_modifiers/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa111_textBox1_modifiers
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public int x;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "x değişkeninin değeri= " + x.ToString();
+
+        }
+    }
+}
