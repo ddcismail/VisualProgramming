205c3f4e5b3cc2df325ecd977e257625847d778c
ismaildedec <ismaildedec@gmail.com>
2021-12-23 11:43:33 +0300

    ısDisposed örneği eklendi.

---
+++ ısDisposed/sayfa108-ısDisposed/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa108_ısDisposed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 bizim_ikinci_formumuz;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((bizim_ikinci_formumuz == null) || (bizim_ikinci_formumuz.IsDisposed))
+            {
+                bizim_ikinci_formumuz = new Form2();
+                bizim_ikinci_formumuz.Show();
+            }
+            else
+            {
+                bizim_ikinci_formumuz.Activate();
+            }
+
+        }
+    }
+}
