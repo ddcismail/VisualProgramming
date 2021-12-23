6840f5a5b2785724a231baed2351802a7cebe0ba
ismaildedec <ismaildedec@gmail.com>
2021-12-23 11:55:10 +0300

    Formlar arası erişim sağlanan örnek eklendi.

---
+++ formlar_Arasi_Erisim/sayfa109-formlar_Arasi_Erisim/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa109_formlar_Arasi_Erisim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 bizim_ikinci_formumuz = new Form2();
+            bizim_ikinci_formumuz.x = 100;
+            bizim_ikinci_formumuz.Show();
+
+        }
+    }
+}
