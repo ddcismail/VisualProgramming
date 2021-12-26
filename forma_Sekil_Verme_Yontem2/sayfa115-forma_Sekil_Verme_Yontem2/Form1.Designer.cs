e769e459940c6425177e29790b38f39b83aa2a59
ismaildedec <ismaildedec@gmail.com>
2021-12-26 14:27:18 +0300

    Forma şekil verme yöntemleri02 eklendi.

---
+++ forma_Sekil_Verme_Yontem2/sayfa115-forma_Sekil_Verme_Yontem2/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace sayfa115_forma_Sekil_Verme_Yontem2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(31, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ forma_Sekil_Verme_Yontem2/sayfa115-forma_Sekil_Verme_Yontem2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa115_forma_Sekil_Verme_Yontem2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Point tiklanan_nokta;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath grafik_sekil;
+            grafik_sekil = new System.Drawing.Drawing2D.GraphicsPath();
+
+            grafik_sekil.AddRectangle(new Rectangle(0, 0, this.ClientSize.Width, 90));
+            grafik_sekil.AddString("Bizim Formumuz", new FontFamily("Tahoma"), (int)FontStyle.Bold, (float)72, new PointF(0, 100), new StringFormat(StringFormat.GenericDefault));
+            grafik_sekil.AddRectangle(new Rectangle(0, this.ClientSize.Height - 90, this.ClientSize.Width, 90));
+
+            this.Region = new Region(grafik_sekil);
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            tiklanan_nokta = new Point(e.X, e.Y);
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point koordinatlar;
+                koordinatlar = Control.MousePosition;
+                koordinatlar.Offset(-tiklanan_nokta.X, -tiklanan_nokta.Y);
+                this.Location = koordinatlar;
+            }
+
+        }
+    }
+}
