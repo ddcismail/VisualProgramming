92693f6b10e1fcd2e76174cd59bf08db6233a055
ismaildedec <ismaildedec@gmail.com>
2021-12-27 15:08:01 +0300

    MDI formlar01 örneği eklendi.

---
+++ MDI_Formlar01/sayfa117-MDI_Formlar01/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa117_MDI_Formlar01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 cocuk_form = new Form2();
+            cocuk_form.MdiParent = this;
+            cocuk_form.Show();
+
+        }
+    }
+}
