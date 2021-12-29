77cb259178e9ec5a3db9c47a2da212c7d4788c1b
ismaildedec <ismaildedec@gmail.com>
2021-12-29 11:10:10 +0300

    Read Only örneği eklendi.

---
+++ readOnly/sayfa124-readOnly/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace sayfa124_readOnly
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_soyadiniz = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_adiniz = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_tamam = new System.Windows.Forms.Button();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_soyadiniz
+            // 
+            this.txt_soyadiniz.Location = new System.Drawing.Point(155, 205);
+            this.txt_soyadiniz.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_soyadiniz.Name = "txt_soyadiniz";
+            this.txt_soyadiniz.Size = new System.Drawing.Size(172, 20);
+            this.txt_soyadiniz.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(36, 205);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Soyadınız";
+            // 
+            // txt_adiniz
+            // 
+            this.txt_adiniz.Location = new System.Drawing.Point(155, 145);
+            this.txt_adiniz.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_adiniz.Name = "txt_adiniz";
+            this.txt_adiniz.Size = new System.Drawing.Size(172, 20);
+            this.txt_adiniz.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(67, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Adınız";
+            // 
+            // btn_tamam
+            // 
+            this.btn_tamam.Location = new System.Drawing.Point(155, 80);
+            this.btn_tamam.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_tamam.Name = "btn_tamam";
+            this.btn_tamam.Size = new System.Drawing.Size(172, 33);
+            this.btn_tamam.TabIndex = 9;
+            this.btn_tamam.Text = "Tamam";
+            this.btn_tamam.UseVisualStyleBackColor = true;
+            this.btn_tamam.Click += new System.EventHandler(this.btn_tamam_Click);
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Location = new System.Drawing.Point(155, 33);
+            this.txt_sifre.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(172, 20);
+            this.txt_sifre.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(67, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Şifre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_soyadiniz);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_adiniz);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_tamam);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_soyadiniz;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_adiniz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_tamam;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ readOnly/sayfa124-readOnly/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa124_readOnly
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_sifre.PasswordChar = '*';
+            txt_adiniz.ReadOnly = true;
+            txt_soyadiniz.ReadOnly = true;
+
+        }
+
+        private void btn_tamam_Click(object sender, EventArgs e)
+        {
+            if (txt_sifre.Text == "1357")
+            {
+                txt_adiniz.ReadOnly = false;
+                txt_soyadiniz.ReadOnly = false;
+            }
+            else
+            {
+                txt_adiniz.ReadOnly = true;
+                txt_soyadiniz.ReadOnly = true;
+            }
+
+        }
+    }
+}
