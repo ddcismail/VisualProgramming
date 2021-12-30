5f169f3940d59ec291bde428f987ba9af04276ad
ismaildedec <ismaildedec@gmail.com>
2021-12-30 21:35:51 +0300

    Item add range örneği eklendi.

---
+++ Items.AddRange/sayfa144-Items.AddRange/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa144_Items.AddRange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] gunler = { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+            listBox1.Items.AddRange(gunler);
+
+        }
+    }
+}
