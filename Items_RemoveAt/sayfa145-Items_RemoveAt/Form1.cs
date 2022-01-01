8b15b718203dfee1600a04473ac0f5cc67472fad
ismaildedec <ismaildedec@gmail.com>
2022-01-01 12:32:52 +0300

    Items removeat örneği ekelndi.

---
+++ Items_RemoveAt/sayfa145-Items_RemoveAt/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa145_Items_RemoveAt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] gunler = { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+            listBox1.Items.AddRange(gunler);
+            listBox1.Items.Insert(3, "Haftanın Günleri");//Ekleme
+            listBox1.Items.RemoveAt(2);//Silme
+
+        }
+    }
+}
