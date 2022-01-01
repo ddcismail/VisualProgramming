e4b494569b59f7a7c009ef40db011ac871bd1dd8
ismaildedec <ismaildedec@gmail.com>
2022-01-01 12:33:38 +0300

    Itmes Insert örneği eklendi.

---
+++ Itmes_Insert/sayfa145-Itmes_Insert/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa145_Itmes_Insert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] gunler = { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+            listBox1.Items.AddRange(gunler);
+            listBox1.Items.Insert(0, "Haftanın Günleri");
+            listBox1.Items.Insert(1, "*****************");
+
+        }
+    }
+}
