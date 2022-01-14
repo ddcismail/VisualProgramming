7aab9237cd8322f2ee9f6c7309544dcfa2d51973
ismaildedec <ismaildedec@gmail.com>
2022-01-14 10:31:25 +0300

    Date time picker örneği eklendi.

---
+++ DateTimePicker/sayaf178_DateTimePicker/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace sayaf178_DateTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker_dogum_tarihi = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker_kayit_tarihi = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_mezuniyet_tarihi = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(29, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı Soyadı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baba Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(29, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Doğum Yeri";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(286, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Doğum Tarihi";
+            // 
+            // dateTimePicker_dogum_tarihi
+            // 
+            this.dateTimePicker_dogum_tarihi.Location = new System.Drawing.Point(410, 44);
+            this.dateTimePicker_dogum_tarihi.Name = "dateTimePicker_dogum_tarihi";
+            this.dateTimePicker_dogum_tarihi.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_dogum_tarihi.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(286, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kayıt Tarihi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(286, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Mezuniyet Tarihi";
+            // 
+            // dateTimePicker_kayit_tarihi
+            // 
+            this.dateTimePicker_kayit_tarihi.Location = new System.Drawing.Point(410, 95);
+            this.dateTimePicker_kayit_tarihi.Name = "dateTimePicker_kayit_tarihi";
+            this.dateTimePicker_kayit_tarihi.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_kayit_tarihi.TabIndex = 10;
+            // 
+            // dateTimePicker_mezuniyet_tarihi
+            // 
+            this.dateTimePicker_mezuniyet_tarihi.Location = new System.Drawing.Point(410, 150);
+            this.dateTimePicker_mezuniyet_tarihi.Name = "dateTimePicker_mezuniyet_tarihi";
+            this.dateTimePicker_mezuniyet_tarihi.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_mezuniyet_tarihi.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_sil);
+            this.groupBox1.Controls.Add(this.btn_ekle);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dateTimePicker_mezuniyet_tarihi);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dateTimePicker_kayit_tarihi);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dateTimePicker_dogum_tarihi);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(43, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(619, 232);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Öğrencinin";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(43, 293);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(611, 186);
+            this.listBox1.TabIndex = 13;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ekle.Location = new System.Drawing.Point(123, 194);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(169, 23);
+            this.btn_ekle.TabIndex = 12;
+            this.btn_ekle.Text = "Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_sil.Location = new System.Drawing.Point(318, 194);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(169, 23);
+            this.btn_sil.TabIndex = 13;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 503);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_dogum_tarihi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_kayit_tarihi;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_mezuniyet_tarihi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_ekle;
+    }
+}
+
---
+++ DateTimePicker/sayaf178_DateTimePicker/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayaf178_DateTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker_dogum_tarihi.Format = DateTimePickerFormat.Short;
+            dateTimePicker_kayit_tarihi.Format = DateTimePickerFormat.Short;
+            dateTimePicker_mezuniyet_tarihi.Format = DateTimePickerFormat.Short;
+
+            dateTimePicker_dogum_tarihi.MaxDate = DateTime.Now;
+            dateTimePicker_kayit_tarihi.MaxDate = DateTime.Now;
+
+            dateTimePicker_mezuniyet_tarihi.ShowCheckBox = true;
+
+            listBox1.Font = new Font("Courier New", 10, FontStyle.Regular);
+        }
+        Boolean mezun_oldu;
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            string ogrenci_adsoyad, ogrenci_babaadi, ogrenci_dogumyeri;
+            DateTime dogum_tarihi, kayit_tarihi, mezuniyet_tarihi;
+
+            ogrenci_adsoyad = textBox1.Text;
+            ogrenci_babaadi = textBox2.Text;
+            ogrenci_dogumyeri = textBox3.Text;
+
+            dogum_tarihi = dateTimePicker_dogum_tarihi.Value;
+            kayit_tarihi = dateTimePicker_kayit_tarihi.Value;
+            mezuniyet_tarihi = dateTimePicker_mezuniyet_tarihi.Value;
+
+            if (dateTimePicker_mezuniyet_tarihi.Checked)
+            {
+                mezun_oldu = true;
+            }
+            else
+            {
+                mezun_oldu = false;
+            }
+
+            if (dogum_tarihi >= kayit_tarihi)
+            {
+                MessageBox.Show("Doğum Tarihi Veya Kayit Tarihi Yanlış Girişilmiş");
+                return;
+            }
+
+            if (mezun_oldu)
+            {
+                if (kayit_tarihi > mezuniyet_tarihi)
+                {
+                    MessageBox.Show("Kayit Tarihi Veya Mezuniyet Tarihi Yanlış Girişilmiş");
+                    return;
+                }
+            }
+
+            listBox1.Items.Add(ogrenci_adsoyad +
+                ogrenci_babaadi.PadLeft(10) +
+                ogrenci_dogumyeri.PadLeft(10)+
+                dogum_tarihi.ToShortDateString().PadLeft(15)+
+                kayit_tarihi.ToShortDateString().PadLeft(15) +
+                mezuniyet_tarihi.ToShortDateString().PadLeft(15) );
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+    }
+}
