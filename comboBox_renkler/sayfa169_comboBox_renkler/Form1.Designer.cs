3e1d2857c269db23d837d8ccfd27eb8a7fcb0cdf
ismaildedec <ismaildedec@gmail.com>
2022-01-17 19:38:11 +0300

    comboBox renkler ile ilgili örnek eklendi.

---
+++ comboBox_renkler/sayfa169_comboBox_renkler/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace sayfa169_comboBox_renkler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.comboBox1_DrawItem);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 332);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ comboBox_renkler/sayfa169_comboBox_renkler/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa169_comboBox_renkler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] renkler_listesi = { "Kırmızı", "Yeşil", "Sarı", "Mavi", "Pembe", "Kahverengi" };
+            comboBox1.Items.AddRange(renkler_listesi);
+        }
+
+        private void comboBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Color renkler = Color.Black;
+            string eleman;
+            eleman = comboBox1.Items[e.Index].ToString();
+
+            if (eleman=="Kırmızı")
+            {
+                renkler = Color.Red;
+            }
+            else if (eleman == "Yeşil")
+            {
+                renkler = Color.Green;
+            }
+            else if (eleman == "Sarı")
+            {
+                renkler = Color.Yellow;
+            }
+            else if (eleman == "Mavi")
+            {
+                renkler = Color.Blue;
+            }
+            else if (eleman == "Pembe")
+            {
+                renkler = Color.Pink;
+            }
+            else if (eleman == "Kahverengi")
+            {
+                renkler = Color.Brown;
+            }
+
+            if (e.State==DrawItemState.Selected)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Turquoise), e.Bounds.Left, e.Bounds.Top, e.Bounds.Width, e.Bounds.Height);
+            }
+            else
+            {
+                e.Graphics.FillRectangle(new SolidBrush(renkler), e.Bounds.Left, e.Bounds.Top, e.Bounds.Width, e.Bounds.Height);
+            }
+
+            e.Graphics.DrawString(eleman,e.Font,new SolidBrush(Color.White),e.Bounds.Left,e.Bounds.Top);
+
+            e.DrawFocusRectangle();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color renkler = Color.Black;
+            string eleman;
+            eleman = comboBox1.SelectedItem.ToString();
+
+            if (eleman == "Kırmızı")
+            {
+                renkler = Color.Red;
+            }
+            else if (eleman == "Yeşil")
+            {
+                renkler = Color.Green;
+            }
+            else if (eleman == "Sarı")
+            {
+                renkler = Color.Yellow;
+            }
+            else if (eleman == "Mavi")
+            {
+                renkler = Color.Blue;
+            }
+            else if (eleman == "Pembe")
+            {
+                renkler = Color.Pink;
+            }
+            else if (eleman == "Kahverengi")
+            {
+                renkler = Color.Brown;
+            }
+            this.BackColor = renkler;
+            comboBox1.BackColor = renkler;
+            comboBox1.ForeColor = Color.White;
+        }
+    }
+}
