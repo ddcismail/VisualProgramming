8403601014adba519138f51c036f1a0f1e8a6ff1
ismaildedec <ismaildedec@gmail.com>
2022-01-19 14:36:13 +0300

    Domain Up Down örneği eklendi.

---
+++ DomainUpDown/sayfa173_DomainUpDown/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace sayfa173_DomainUpDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(98, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Renk";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(101, 134);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(209, 159);
+            this.panel1.TabIndex = 2;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(159, 68);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDown1.TabIndex = 3;
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 370);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+    }
+}
+
---
+++ DomainUpDown/sayfa173_DomainUpDown/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa173_DomainUpDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] renkler = { "Kırmızı", "Sarı", "Yeşil", "Mavi", "Pembe", "Kahverengi", "Siyah" };
+            domainUpDown1.Items.AddRange(renkler);
+            domainUpDown1.Wrap = true;
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            if (domainUpDown1.SelectedIndex>=0)
+            {
+                if (domainUpDown1.SelectedItem.ToString() =="Kırmızı")
+                {
+                    panel1.BackColor = Color.Red;
+                }
+                if (domainUpDown1.SelectedItem.ToString() == "Sarı")
+                {
+                    panel1.BackColor = Color.Yellow;
+                }
+                if (domainUpDown1.SelectedItem.ToString() == "Yeşil")
+                {
+                    panel1.BackColor = Color.Green;
+                }
+                if (domainUpDown1.SelectedItem.ToString() == "Mavi")
+                {
+                    panel1.BackColor = Color.Blue;
+                }
+                if (domainUpDown1.SelectedItem.ToString() == "Pembe")
+                {
+                    panel1.BackColor = Color.Pink;
+                }
+                if (domainUpDown1.SelectedItem.ToString() == "Kahverengi")
+                {
+                    panel1.BackColor = Color.Brown;
+                }
+                if (domainUpDown1.SelectedItem.ToString() == "Siyah")
+                {
+                    panel1.BackColor = Color.Black;
+                }
+            }
+        }
+    }
+}
