89c28ef15134923febeb986491daa810eceb7541
ismaildedec <ismaildedec@gmail.com>
2022-01-26 14:18:51 +0300

    Progress Bar örneği eklendi.

---
+++ ProgressBar/sayfa175_ProgressBar/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace sayfa175_ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(146, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(51, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(131, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sırala";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(59, 357);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(250, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ProgressBar/sayfa175_ProgressBar/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa175_ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 500; i++)
+            {
+                int rastgele_sayi = r.Next(100);
+                listBox1.Items.Add(rastgele_sayi.ToString());
+            }
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 500;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime baslama_zamani, bitis_zamani;
+            TimeSpan fark;
+            this.Text = "Lütfen Bekleyiniz";
+            baslama_zamani = DateTime.Now;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                for (int j = i; j < listBox1.Items.Count; j++)
+                {
+                    int sayi1, sayi2;
+                    sayi1 = Convert.ToInt16(listBox1.Items[i]);
+                    sayi2 = Convert.ToInt16(listBox1.Items[j]);
+                    if (sayi2 > sayi1)
+                    {
+                        int c = sayi2;
+                        sayi2 = sayi1;
+                        sayi1 = c;
+                        listBox1.Items[i] = sayi1.ToString();
+                        listBox1.Items[j] = sayi2.ToString();
+                    }
+                }
+                progressBar1.Value = 1;
+            }
+            bitis_zamani = DateTime.Now;
+            fark = bitis_zamani - baslama_zamani;
+            this.Text = "İşlem süresi " + fark.ToString().Substring(0, 8);
+        }
+    }
+}
